3c8a94363574853a0c8e0655a971fd17ce4d99d8
LuanaTito <111540897+LuanaTito@users.noreply.github.com>
2024-02-20 22:28:10 -0300

    js

---
+++ js/jsinteracao.js
@@ -0,0 +1,27 @@
+
+//Humburguer
+function clickMenu(){
+    // itens.style.display = 'block'
+    if(itens.style.display == 'block'){
+        itens.style.display = 'none'
+    }else{
+        itens.style.display = 'block'
+    }
+    
+}
+
+
+
+//MODAL
+// Fechamento do Modal
+var span = document.getElementsByClassName("close")[0];
+
+// Ao clicar no botao o modal aparece
+function modalClick() {
+  mModal.style.display = "block";
+}
+
+// quando clicar no span o modal fecha
+span.onclick = function() {
+  mModal.style.display = "none";
+}
